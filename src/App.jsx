b7d0b64cf6bf168321df861874e7a5766dbb223d
CharlyMannion <charlottemannion100@gmail.com>
2020-12-29 15:46:37 +0000

    builds out an error display component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import NavBar from './components/NavBar';
 import OptionsBar from './components/OptionsBar';
 import ShoeList from './pages/ShoeList';
 import SingleShoe from './pages/SingleShoe'
-// import ErrorDisplay from './components/ErrorDisplay';
+import ErrorDisplay from './components/ErrorDisplay';
 
 class App extends Component {
 
@@ -21,7 +21,7 @@ class App extends Component {
          <Router>
              <ShoeList path="/" />
              <SingleShoe path="/shoes/:shoe_id" />
-             {/* <ErrorDisplay default status={404} message="This page does not exist"/> */}
+             <ErrorDisplay default status={404} message="This page does not exist"/>
          </Router>
          <Footer />
       </div>
