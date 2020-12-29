1c3c4910a1bbb3019303e1bfbec88c99d07d6b85
CharlyMannion <charlottemannion100@gmail.com>
2020-12-29 18:18:21 +0000

    comments out avatr urls until can source better images of shoes

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -4,7 +4,7 @@ import { Link } from '@reach/router';
 const ShoeCard = (props) => {
     return (
         <section className='shoeCard'>
-            <img className="avatar" src={props.avatar_url} alt="missing shoe image..."></img>
+            {/* <img className="avatar" src={props.avatar_url} alt="missing shoe image..."></img> */}
             <Link to={`/shoe/${props.shoe_id}`} style={{ color: 'inherit', textDecoration: 'none' }}>
                 <h3>{props.name}</h3>
             </Link>
